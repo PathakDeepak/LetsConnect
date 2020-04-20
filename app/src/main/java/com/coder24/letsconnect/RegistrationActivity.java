543054ae780fcdff76bb03d46455f09d0c22f173
DeepakPathak <pathakdeepak024@gmail.com>
2020-04-20 15:26:21 +0530

    Initial Commit
    Added registration code

---
+++ app/src/main/java/com/coder24/letsconnect/RegistrationActivity.java
@@ -0,0 +1,201 @@
+package com.coder24.letsconnect;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.FirebaseTooManyRequestsException;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthProvider;
+import com.hbb20.CountryCodePicker;
+
+import java.util.concurrent.TimeUnit;
+
+public class RegistrationActivity extends AppCompatActivity {
+
+    private CountryCodePicker ccp;
+    private EditText phoneText;
+    private EditText codeText;
+    private Button continueAndNextBtn;
+    private String checker = "", phoneNumber = "";
+    private RelativeLayout relativeLayout;
+
+    //For getting auth function from firebase
+    private PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallbacks;
+    private FirebaseAuth mAuth;
+    private String mVerificationId;
+    private PhoneAuthProvider.ForceResendingToken mResendToken;
+
+    private ProgressDialog loadingBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+
+        //initialize Firebase AuthProvider
+        mAuth = FirebaseAuth.getInstance();
+
+        //ProgressDialogBar
+        loadingBar = new ProgressDialog(this);
+
+        //initialize the elements
+        phoneText = findViewById(R.id.phoneText);
+        codeText = findViewById(R.id.codeText);
+        continueAndNextBtn = findViewById(R.id.continueNextButton);
+        relativeLayout = findViewById(R.id.phoneAuth);
+
+        ccp = (CountryCodePicker) findViewById(R.id.ccp);
+        ccp.registerCarrierNumberEditText(phoneText);
+
+        continueAndNextBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(continueAndNextBtn.getText().equals("Submit") || checker.equals("Code Sent")){
+
+                    String verificationCode = codeText.getText().toString();
+
+                    if(verificationCode.equals("")){
+                        Toast.makeText(RegistrationActivity.this, "Please enter verification code.", Toast.LENGTH_SHORT).show();
+                    }
+                    else{
+                        //loading bar show while code verification
+                        loadingBar.setTitle("Code Verification");
+                        loadingBar.setMessage("Please wait, while we are verifying your code..");
+                        loadingBar.setCanceledOnTouchOutside(false);
+                        loadingBar.show();
+
+                        PhoneAuthCredential credential = PhoneAuthProvider.getCredential(mVerificationId, verificationCode);
+                        signInWithPhoneAuthCredential(credential);
+                    }
+                }
+                else{
+                    phoneNumber = ccp.getFullNumberWithPlus();
+                    if(!phoneNumber.equals("")){
+
+                        //loading bar show while phone number verification
+                        loadingBar.setTitle("Phone Number Verification");
+                        loadingBar.setMessage("Please wait, while we are verifying your phone number");
+                        loadingBar.setCanceledOnTouchOutside(false);
+                        loadingBar.show();
+
+                        //send a code from firebase to phone number
+                        //pass their phone number to the PhoneAuthProvider.
+                        //verifyPhoneNumber method to request that Firebase verify the user's phone number.
+                        PhoneAuthProvider.getInstance().verifyPhoneNumber(
+                                phoneNumber,        // Phone number to verify
+                                60,                 // Timeout duration
+                                TimeUnit.SECONDS,   // Unit of timeout
+                                RegistrationActivity.this,               // Activity (for callback binding)
+                                mCallbacks);        // OnVerificationStateChangedCallbacks
+
+                    }
+                    else{
+                        Toast.makeText(RegistrationActivity.this, "Please write valid phone number.", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
+
+        //function called if verification failed somehow either wrong code or something
+        /*
+        * When you call PhoneAuthProvider.verifyPhoneNumber,
+        * you must also provide an instance of OnVerificationStateChangedCallbacks,
+        * which contains implementations of the callback functions that handle the results of the request.
+        * */
+        mCallbacks = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+            @Override
+            public void onVerificationCompleted(@NonNull PhoneAuthCredential phoneAuthCredential) {
+                // This callback will be invoked in two situations:
+                // 1 - Instant verification. In some cases the phone number can be instantly
+                //     verified without needing to send or enter a verification code.
+                // 2 - Auto-retrieval. On some devices Google Play services can automatically
+                //     detect the incoming verification SMS and perform verification without
+                //     user action.
+                signInWithPhoneAuthCredential(phoneAuthCredential);
+
+            }
+
+            @Override
+            public void onVerificationFailed(@NonNull FirebaseException e) {
+                // This callback is invoked in an invalid request for verification is made,
+                // for instance if the the phone number format is not valid.
+
+                Toast.makeText(RegistrationActivity.this, "Invalid Phone Number...", Toast.LENGTH_SHORT).show();
+
+                loadingBar.dismiss();
+                relativeLayout.setVisibility(View.VISIBLE);
+
+                continueAndNextBtn.setText("Continue");
+                codeText.setVisibility(View.GONE);
+                if (e instanceof FirebaseAuthInvalidCredentialsException) {
+                    // Invalid request
+                    // ...
+                } else if (e instanceof FirebaseTooManyRequestsException) {
+                    // The SMS quota for the project has been exceeded
+                    // ...
+                }
+            }
+
+            @Override
+            public void onCodeSent(@NonNull String s, @NonNull PhoneAuthProvider.ForceResendingToken forceResendingToken) {
+                super.onCodeSent(s, forceResendingToken);
+                // The SMS verification code has been sent to the provided phone number, we
+                // now need to ask the user to enter the code and then construct a credential
+                // by combining the code with a verification ID.
+                mVerificationId = s;
+                mResendToken = forceResendingToken;
+
+                relativeLayout.setVisibility(View.GONE);
+                checker = "Code Sent";
+                continueAndNextBtn.setText("Submit");
+                codeText.setVisibility(View.VISIBLE);
+
+                loadingBar.dismiss();
+                Toast.makeText(RegistrationActivity.this, "Code is sent..", Toast.LENGTH_SHORT).show();
+            }
+        };
+
+    }
+
+    private void signInWithPhoneAuthCredential(PhoneAuthCredential credential) {
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+
+                            loadingBar.dismiss();
+                            Toast.makeText(RegistrationActivity.this, "Congratualtions..You'r logged in successfully", Toast.LENGTH_SHORT).show();
+                            sendUserToMainActivity();
+                        } else {
+                            // Sign in failed, display a message and update the UI
+                            loadingBar.dismiss();
+                            Toast.makeText(RegistrationActivity.this, "Error: "+task.getException().toString(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    private void sendUserToMainActivity(){
+        startActivity(new Intent(RegistrationActivity.this, MainActivity.class));
+        finish();
+    }
+}
